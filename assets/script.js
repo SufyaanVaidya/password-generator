787dfcf578e494250073903b7172914f6e77ea49
Sufyaan Vaidya <vaidyasufyaan@gmail.com>
2022-06-22 19:18:41 -0400

    added event listener and some var's

--- assets/script.js
+++ assets/script.js
@@ -4,13 +4,12 @@ var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
 var special = ["!", "@", "#", "$", "%", "&"]
 var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
 var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
-var password;
-var rdPassword;
 var confirmLength = "";
 var confirmNumbers;
 var confirmSpecial;
 var confirmLower;
 var confirmUpper;
+var picked;
 
 function generatePassword() {
   confirmLength = (prompt("How long would you like this password?(8-128 characters.)"));
@@ -27,7 +26,7 @@ function generatePassword() {
   confirmLower = confirm("Please click OK to confirm if you would like to use lowercase");
   confirmUpper = confirm("Please click OK to confirm if you would like to use uppercase");
 
-  if (confirmNumber === false && confirmSpecial === false && confirmLower === false && confirmUpper === false) {
+  if (confirmNumbers === false && confirmSpecial === false && confirmLower === false && confirmUpper === false) {
     alert("One option must be selected");
       confirmNumbers = confirm("Please click OK to confirm if you would like to use numbers");
       confirmSpecial = confirm("Please click OK to confirm if you would like to use special characters");
@@ -35,20 +34,8 @@ function generatePassword() {
       confirmUpper = confirm("Please click OK to confirm if you would like to use uppercase");
   }
 
-  if (confirmNumbers) {
-    password = password.concat(numbers)
-  } else if (confirmSpecial) {
-    password = password.concat(special)
-  } else if (confirmLower) {
-    password = password.concat(lowerCase)
-  } else if (confirmUpper) {
-    password = password.concat(upperCase)
-  }
+  if (confirmNumbers && confirmSpecial && confirmLower && confirmUpper) {
 
-  console.log(password)
-
-  for (var i = 0; i < confirmLength; i++) {
-    password = password +
   }
 }
 // Write password to the #password input
@@ -63,3 +50,7 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+generateBtn.addEventListener("click", function () {
+  pass = generatePassword();
+  document.getElementById("password").placeholer = pass;
+});
